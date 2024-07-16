cb5fdafbaa213122d2495cd4d3dd41075cf27c85
neomag <neomag@gmail.com>
2024-07-16 21:20:10 +0300

    change tag

--- helm/make_ingress.sh
+++ helm/make_ingress.sh
@@ -1,3 +1,6 @@
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm install ingress-nginx/ingress-nginx --generate-name -n ns1
+#helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+#helm install ingress-nginx/ingress-nginx --generate-name -n ns1
+
+helm install my-release oci://ghcr.io/nginxinc/charts/nginx-ingress --version 1.3.1  -n ns1
+
 
