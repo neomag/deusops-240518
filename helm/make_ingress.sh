9545c4293c7abe89270bb5d0706d853298fe6e1a
neomag <neomag@gmail.com>
2024-07-17 01:01:30 +0300

    v

--- helm/make_ingress.sh
+++
@@ -1,6 +0,0 @@
-#helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-#helm install ingress-nginx/ingress-nginx --generate-name -n ns1
-
-helm install my-release oci://ghcr.io/nginxinc/charts/nginx-ingress --version 1.3.1  -n ns1
-
-
